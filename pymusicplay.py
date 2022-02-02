a7a44bfa0bcc9dbb8df303c754e3cb0c995ec0ec
Joel York <Joel_York@yahoo.com>
2022-02-01 23:24:33 -0600

    Added some funtions.

--- pymusicplay.py
+++ pymusicplay.py
@@ -4,19 +4,46 @@ import tkinter as tk
 from tkinter import Place, ttk, filedialog
 from pygame import mixer
 
+def play():
+    mixer.music.load(play_list.get(tk.ACTIVE))
+    var.set(play_list.get(tk.ACTIVE))
+    mixer.music.play()
+
+def stop():
+    mixer.music.stop()
+        
+def pause():
+    mixer.music.pause()
+    
+def unpause():
+    mixer.music.unpause()
+
 # Create main window
-root = tk.Tk()
+music_player = tk.Tk()
 mixer.init()
-root.geometry('300x160')
-root.title('Mini Music Player')
-root.configure(bg='white')
-s = ttk.Style(root)
+music_player.geometry('300x160')
+music_player.title('Mini Music Player')
+music_player.configure(bg='white')
+s = ttk.Style(music_player)
 s.theme_use('vista')
 
-root.mainloop()
+music_player.mainloop()
 
 # Add buttons
-ttk.Button(root, text='Play', width=10. command=play_song()).Place(x=10, y=10)
-ttk.Button(root, text='Play', width=10. command=play_song()).Place(x=10, y=10)
-ttk.Button(root, text='Play', width=10. command=play_song()).Place(x=10, y=10)
-ttk.Button(root, text='Play', width=10. command=play_song()).Place(x=10, y=10)
+ttk.Button(music_player, text='Play', width=10, command = play).Place(x=10, y=10)
+ttk.Button(music_player, text='Stop', width=10, command = stop).Place(x=10, y=40)
+ttk.Button(music_player, text='Pause', width=10, command = pause).Place(x=10, y=70)
+ttk.Button(music_player, text='Unpause', width=10, command = unpause).Place(x=10, y=100)
+
+directory = filedialog.askdirectory(title='Pick Music Directory', mustexist=True)
+os.chdir(directory) #it permits to chenge the current dir
+song_list = os.listdir() #it returns the list of files song
+
+var = tk.StringVar() 
+song_title = tk.Label(music_player, font='Helvetica 12 bold', textvariable=var)
+
+play_list = tk.Listbox(music_player, font='Helvetica 12 bold', bg='yellow', selectmode=tk.SINGLE)
+for item in song_list:
+    pos = 0
+    play_list.insert(pos, item)
+    pos += 1
